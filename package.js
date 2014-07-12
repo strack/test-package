671ee87b139b079bc5374694858cfb552a919722
Isaac Strack <strack@gmail.com>
2014-07-12 01:09:11 -0600

    testing for match failed error

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "A Test package",
-    version: "1.0.2",
+    version: "1.0.3",
     githubUrl: "https://github.com/strack/test-package.git"
 });
 
@@ -8,7 +8,9 @@ Package.on_use(function (api) {
     api.export('TestPack'); // `api.export` introduced in Meteor 0.6.5
     api.add_files("test-package.js", "client");
     api.add_files("server-test.js","server");
+    api.versionsFrom('METEOR-CORE@0.9.0-preview');
 });
+
 
 Npm.depends({
     "colors": "0.6.2"
